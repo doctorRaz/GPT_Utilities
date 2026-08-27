a79cd923eda19ef9bb69bb2f21b7156030766785
doctorRAZ_h <doctorRAZ@yandex.ru>
2026-08-27 20:32:15 +0300

    красим консоль

---
+++ Archivist/Services/ConsoleWriter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace dRz.GPT_Utilities.Archivist.Services
+{
+    internal static class ConsoleWriter
+    {
+        public static void WriteLine(string message, ConsoleColor color)
+        {
+            ConsoleColor previousColor = Console.ForegroundColor;
+
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+
+            Console.ForegroundColor = previousColor;
+        }
+
+        public static void Info(string message)
+        {
+
+            WriteLine(message, ConsoleColor.Gray);
+        }
+
+        public static void Success(string message)
+        {
+            WriteLine(message, ConsoleColor.Green);
+        }
+
+        public static void Update(string message)
+        {
+            WriteLine(message, ConsoleColor.Yellow);
+        }
+
+        public static void Warning(string message)
+        {
+            WriteLine(message, ConsoleColor.DarkYellow);
+        }
+
+        public static void Error(string message)
+        {
+            WriteLine(message, ConsoleColor.Red);
+        }
+    }
+}
