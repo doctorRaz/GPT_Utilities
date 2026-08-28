77d089a0a7cfbae608eb77fd861ce3df88ce0dcc
doctorRAZ_h <doctorRAZ@yandex.ru>
2026-08-28 06:50:54 +0300

    draft

--- Archivist/_Draft/GPT_Archivist.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace dRz.GPT_Utilities.GPT_Archivist
-{
-    internal class GPT_Archivist
-    {
-        public GPT_Archivist()
-        {
-        }
-    }
-}
--- Archivist/_Draft/MetadataWriter.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace dRz.GPT_Utilities.GPT_Archivist
-{
-    internal class MetadataWriter
-    {
-        //https://chatgpt.com/c/6a8f26c6-2ae0-83eb-a048-57f9270485a8
-    }
-}
