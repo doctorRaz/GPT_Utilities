a789a68f93db0be9bd02e3cc2872f12ffe917c86
doctorRAZ_h <doctorRAZ@yandex.ru>
2026-08-28 23:00:13 +0300

    склонения для чисел
    всегда мечтал сделать в два слова

---
+++ Archivist/PluralExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace dRz.GPT_Utilities.Archivist
+{
+    namespace dRz.GPT_Utilities.Archivist
+    {
+        public static class PluralExtensions
+        {
+            public static string Of(this int number, PluralForms forms)
+            {
+                int value = Math.Abs(number);
+
+                string word;
+
+                if (value % 100 is >= 11 and <= 19)
+                {
+                    word = forms.Many;
+                }
+                else
+                {
+                    word = (value % 10) switch
+                    {
+                        1 => forms.One,
+                        2 or 3 or 4 => forms.Few,
+                        _ => forms.Many
+                    };
+                }
+
+                return $"{number} {word}";
+            }
+        }
+
+        public readonly record struct PluralForms(
+            string One,
+            string Few,
+            string Many);
+
+        
+    }
+}
---
+++ Archivist/Words.cs
@@ -0,0 +1,20 @@
+﻿using dRz.GPT_Utilities.Archivist.dRz.GPT_Utilities.Archivist;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dRz.GPT_Utilities.Archivist
+{
+    public static class Words
+    {
+        public static readonly PluralForms Archives =
+            new("архив", "архива", "архивов");
+
+        public static readonly PluralForms Files =
+            new("файл", "файла", "файлов");
+    }
+
+   
+}
