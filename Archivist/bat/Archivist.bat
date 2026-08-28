92dbf98699710367912cd3a7927f96aad12ed68a
doctorRAZ_h <doctorRAZ@yandex.ru>
2026-08-28 11:06:05 +0300

    chcp 65001
    обязательно
    ```
    @echo off
    chcp 65001 >nul
    setlocal
    ```

--- Archivist/bat/Archivist.bat
+++ Archivist/bat/Archivist.bat
@@ -1,6 +1,6 @@
-REM @echo off
-REM chcp 65001 >nul
-REM setlocal
+@echo off
+chcp 65001 >nul
+setlocal
 
 "d:\@Developers\Programmers\!NET\!GPT_Utilities\bin\Debug\Archivist.exe" -s "d:\@Developers\В работе\GPT_export\chatgpt-export-markdown" -d "d:\@Developers\В работе\Reminder\GPT-export\Markdown+"
 REM "d:\@Developers\Programmers\!NET\!GPT_Utilities\bin\Release\Archivist.exe" -s "d:\@Developers\В работе\GPT_export\chatgpt-export-markdown" -d "d:\@Developers\В работе\Reminder\GPT-export\Markdown+"
